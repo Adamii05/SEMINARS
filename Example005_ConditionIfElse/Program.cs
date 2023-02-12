8ebdb9d51a398573a45a711dbf16a9531502ca0c
Tima <adamii@li.ru>
2023-02-12 21:06:51 +0400

    Condition IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "таня")
+{
+    Console.WriteLine("Привет, Пупсик");
+}
+else
+{
+    Console.Write(username);
+    Console.WriteLine(" Какое же ты чмо");
+}
