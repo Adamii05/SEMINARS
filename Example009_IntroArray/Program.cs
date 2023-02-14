dbeff7d90d73356ef137c4d93faed4afdabc0c5b
Tima <adamii@li.ru>
2023-02-14 16:15:23 +0400

    Решили задачу по нахождению максимума через массив

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//                0  1  2   3   4   5   6  7  - индексы нашего массива      
+int [] MyArray = {11,21,31,41,15,61,17,18,19};
+
+int Best = Max (
+    Max(MyArray[0], MyArray[1], MyArray[2]),
+    Max(MyArray[3], MyArray[4], MyArray[5]),
+    Max(MyArray[6], MyArray[7], MyArray[8])
+);
+
+
+Console.WriteLine(Best);
+
